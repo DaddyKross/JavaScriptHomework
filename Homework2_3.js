a8faefe999e724122e4bc1bc72d69aaa51205e2f
Anastasia Khrenova <101924092+DaddyKross@users.noreply.github.com>
2024-12-05 23:24:08 +0300

    Delete Homework2_3.js

--- Homework2_3.js
+++
@@ -1,7 +0,0 @@
-let number = parseInt(prompt('Введите число:'));
-
-let units = number % 10;
-let tens = Math.floor((number % 100) / 10);
-let hundreds = Math.floor(number / 100);
-
-console.log(`В числе ${number} количество сотен: ${hundreds}, десятков: ${tens}, единиц: ${units}`);
